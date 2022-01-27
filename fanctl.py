3d40824e48460d6ba5e939593fa9afedbb81947a
Ilya Mazlov <mazlov.i.a@gmail.com>
2022-01-27 06:47:40 +0100

    fanctl.py clean up

--- fanctl.py
+++ fanctl.py
@@ -27,8 +27,6 @@ def between(v, lb=None, ub=None):
     if ub is None:
         return v >= lb
     return v >= lb and v <= ub
-# def between(v, lb, ub):
-#     return min(ub, max(lb, v))
 
 def wrongArgsExit():
     print('Wrong arguments. Run script with: fan_pin: int, fan_min_speed: int, fan_pwm: int')
@@ -40,27 +38,6 @@ def getThresholdState(thresholds, curval):
             return i
     return len(thresholds)
 
-def valWithinHysteresis(thresholds, curThreshold, hyst, val):
-    if curThreshold == 0:
-        ret = between(val, thresholds[0] - hyst, thresholds[0])
-        print(' | w/hyst-0: {0}'.format(ret), end='')
-        return ret
-    if curThreshold == len(thresholds):
-        ret = between(val, thresholds[-1], thresholds[-1] + hyst)
-        print(' | w/hyst-l: {0}'.format(ret), end='')
-        return ret
-    ret = between(val, thresholds[curThreshold] - hyst, thresholds[curThreshold]) \
-        or between(val, thresholds[curThreshold - 1], thresholds[curThreshold - 1] + hyst)
-    print(' | w/hyst-b: {0}'.format(ret), end='')
-    return ret
-
-def valsHysteresis(thresholds, curThreshold, hyst, val):
-    if curThreshold == len(thresholds):
-        return (thresholds[-1] - hyst, math.inf)
-    mx = thresholds[curThreshold] + hyst
-    mn = thresholds[curThreshold - 1] - hyst
-    return (mn, mx) if mn <= mx else (-math.inf, mx)
-
 def getSpeedForTempThreshold(tempThresholds, speedThresholds, curTempThreshold):
     if curTempThreshold == 0:
         return 0
@@ -69,21 +46,23 @@ def getSpeedForTempThreshold(tempThresholds, speedThresholds, curTempThreshold):
     return speedThresholds[curTempThreshold - 1]
 
 def readTemperature():
-    # with open('/sys/class/thermal/thermal_zone0/temp', 'r') as f:
-    with open('/home/pi/tmp/temp', 'r') as f:
-        temp = float(f.read())
+    with open('/sys/class/thermal/thermal_zone0/temp', 'r') as f:
+        temp = float(f.read()) * 1e-3
     return temp
+
+def setFanSpeed(tThresholds, sThresholds, state):
+    newSpeed = getSpeedForTempThreshold(tThresholds, sThresholds, state)
+    print('Set speed to {0}%'.format(newSpeed))
+    fan.ChangeDutyCycle(newSpeed)
 
 
 if __name__ == '__main__':
     print('Welcome to Fanctl script!')
 
     # Configuration
-    WAIT_TIME = 1  # [s] Time to wait between each refresh
-    HYSTERESIS = 1  # [°C] Fan speed will change only of the difference of temperature is higher than hysteresis
-    # MIN_CYCLE_DURATION = 3  # [s] Minimum time which need to pass within the same state between temp thresholds
-    TEMP_THRESHOLDS = [50, 55, 65, 70]  # [°C]
-    SPEED_THRESHOLDS = [0, 30, 70, 100]  # [%]
+    WAIT_TIME = 2  # [s] Time to wait between each refresh
+    TEMP_THRESHOLDS = [50, 55, 65]  # [°C]
+    SPEED_THRESHOLDS = [0, 50, 100]  # [%]
 
     if len(sys.argv) < 4:
         wrongArgsExit()
@@ -110,84 +89,24 @@ if __name__ == '__main__':
         sys.exit()
 
     temp = readTemperature()
-    print('[Init] Temp: {0}'.format(temp), end='')
     lastThresholdState = getThresholdState(TEMP_THRESHOLDS, temp)
-    print(' | ind: {0}'.format(lastThresholdState), end='')
-    newSpeed = getSpeedForTempThreshold(TEMP_THRESHOLDS, SPEED_THRESHOLDS, lastThresholdState)
-    fan.ChangeDutyCycle(newSpeed)
-    print(' | change -> {0}'.format(newSpeed))
+    setFanSpeed(TEMP_THRESHOLDS, SPEED_THRESHOLDS, lastThresholdState)
 
     try:
         while 1:
             temp = readTemperature()
-            print('Temp: {0}'.format(temp), end='')
-
             curThresholdState = getThresholdState(TEMP_THRESHOLDS, temp)
-            print(' | ind: {0}'.format(curThresholdState), end='')
 
             if curThresholdState > lastThresholdState:
                 lastThresholdState = curThresholdState
-                newSpeed = getSpeedForTempThreshold(TEMP_THRESHOLDS, SPEED_THRESHOLDS, lastThresholdState)
-                fan.ChangeDutyCycle(newSpeed)
-                print(' | change -> {0}'.format(newSpeed), end='')
+                print('Temp: {0} | '.format(temp), end='')
+                setFanSpeed(TEMP_THRESHOLDS, SPEED_THRESHOLDS, lastThresholdState)
             elif lastThresholdState - curThresholdState > 1:
                 lastThresholdState = curThresholdState + 1
-                newSpeed = getSpeedForTempThreshold(TEMP_THRESHOLDS, SPEED_THRESHOLDS, lastThresholdState)
-                fan.ChangeDutyCycle(newSpeed)
-                print(' | change -> {0}'.format(newSpeed), end='')
-
-            print()
+                print('Temp: {0} | '.format(temp), end='')
+                setFanSpeed(TEMP_THRESHOLDS, SPEED_THRESHOLDS, lastThresholdState)
             time.sleep(WAIT_TIME)
 
-    except KeyboardInterrupt:
-        print("Fan ctrl interrupted by keyboard")
-        GPIO.cleanup()
-        sys.exit()
-
-
-    i = 0
-    cpuTemp = 0
-    fanSpeed = 0
-    cpuTempOld = 0
-    fanSpeedOld = 0
-
-    try:
-        while 1:
-            # Read CPU temperature
-            cpuTempFile = open("/sys/class/thermal/thermal_zone0/temp", "r")
-            cpuTemp = float(cpuTempFile.read()) / 1000
-            cpuTempFile.close()
-
-            # Calculate desired fan speed
-            if abs(cpuTemp - cpuTempOld) > hyst:
-                # Below first value, fan will run at min speed.
-                if cpuTemp < tempSteps[0]:
-                    fanSpeed = speedSteps[0]
-                # Above last value, fan will run at max speed
-                elif cpuTemp >= tempSteps[len(tempSteps) - 1]:
-                    fanSpeed = speedSteps[len(tempSteps) - 1]
-                # If temperature is between 2 steps, fan speed is calculated by linear interpolation
-                else:
-                    for i in range(0, len(tempSteps) - 1):
-                        if (cpuTemp >= tempSteps[i]) and (cpuTemp < tempSteps[i + 1]):
-                            fanSpeed = round((speedSteps[i + 1] - speedSteps[i])
-                                             / (tempSteps[i + 1] - tempSteps[i])
-                                             * (cpuTemp - tempSteps[i])
-                                             + speedSteps[i], 1)
-
-                if fanSpeed != fanSpeedOld:
-                    if (fanSpeed != fanSpeedOld
-                            and (fanSpeed >= fanMinSpeed or fanSpeed == 0)):
-                        fan.ChangeDutyCycle(fanSpeed)
-                        fanSpeedOld = fanSpeed
-                cpuTempOld = cpuTemp
-            
-            # print('Temp: ', cpuTemp, 'Fan: ', fanSpeed, 'Freq: ', fanPWM)
-            # Wait until next refresh
-            time.sleep(WAIT_TIME)
-
-
-    # If a keyboard interrupt occurs (ctrl + c), the GPIO is set to 0 and the program exits.
     except KeyboardInterrupt:
         print("Fan ctrl interrupted by keyboard")
         GPIO.cleanup()
